d7965319a2225cb3c6fa0c7ec0bd35b2fe9c9009
Xiao Fan <xiao.h.fan@gmail.com>
2017-02-05 19:06:50 -0800

    include find='find .' alias

--- aliases.osx.plugin.zsh
+++ aliases.osx.plugin.zsh
@@ -1,3 +1,4 @@
 if [[ "$OSTYPE" == "darwin"* ]]; then
   alias ls='ls -FG'    # append indicator, colorized
+  alias find='find .'  # I usually just want to search in current directory. For explicit directory passing, use '\find'
 fi
