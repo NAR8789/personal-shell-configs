250fa226f4276862f563c70d947ec918fb950b0d
Xiao Fan <xiao.h.fan@gmail.com>
2017-02-05 19:10:59 -0800

    include gg=gitx

--- aliases.osx.plugin.zsh
+++ aliases.osx.plugin.zsh
@@ -1,4 +1,5 @@
 if [[ "$OSTYPE" == "darwin"* ]]; then
   alias ls='ls -FG'    # append indicator, colorized
   alias find='find .'  # I usually just want to search in current directory. For explicit directory passing, use '\find'
+  alias gg=gitx        # so I don't have to remember to type gitx vs gg on osx vs linux
 fi
