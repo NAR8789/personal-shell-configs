94972ee09ec90188fed34f2264bbe5b68b82e4d8
Xiao Fan <xiao.h.fan@gmail.com>
2017-10-03 08:37:44 -0700

    use shrink_path

--- settings.zsh
+++ settings.zsh
@@ -1,4 +1,4 @@
 unsetopt beep
 
 # custom zsh git prompt (requires olivierverdier/zsh-git-prompt)
-PROMPT='%B%F{red}%m%F{default}:%F{cyan}%~%F{default}%b$(git_super_status)%B%#%b '
+PROMPT='%B%F{red}%m%F{default}:%F{cyan}$(shrink_path --tilde --last)%F{default}%b$(git_super_status)%B%#%b '
