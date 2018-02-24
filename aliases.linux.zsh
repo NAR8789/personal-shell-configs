405a9a13abb25063e37ac8fb12de47042c2f9d08
Xiao Fan <xiao.h.fan@gmail.com>
2018-02-24 13:37:45 -0800

    typing systemctl --user over and over again is aggravating

--- aliases.linux.zsh
+++ aliases.linux.zsh
@@ -1,4 +1,6 @@
 if [[ "$OSTYPE" == "linux-gnu" ]]; then
   alias ls='ls -F --color=always'    # append indicator, always colorized
   alias df='df -x tmpfs -x devtmpfs' # hide the weird filesystems in df output
+  alias sc='systemctl'
+  alias scu='systemctl --user'
 fi
