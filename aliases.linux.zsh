72dc62c8e5d91404029107659b4b555725e43271
Xiao Fan <xiao.h.fan@gmail.com>
2018-02-24 13:37:04 -0800

    no longer using wicd

--- aliases.linux.zsh
+++ aliases.linux.zsh
@@ -1,5 +1,4 @@
 if [[ "$OSTYPE" == "linux-gnu" ]]; then
   alias ls='ls -F --color=always'    # append indicator, always colorized
-  alias wicd-client='wicd-client -n' # probably don't want tray daemon if launching wicd from command line
   alias df='df -x tmpfs -x devtmpfs' # hide the weird filesystems in df output
 fi
