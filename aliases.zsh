8e7182dec760a64d1246a24217d4c56affb69287
Xiao Fan <xiao.h.fan@gmail.com>
2018-04-29 16:03:10 -0700

    allow emacs to launch gui by default

--- aliases.zsh
+++ aliases.zsh
@@ -18,7 +18,7 @@ alias tmux=byobu
 
 # I'd like emacs and octave to stay in the terminal if I'm launching from terminal.
 # If I want to open in gui mode, I can open from an external launcher.
-alias emacs='emacs -nw'
+#alias emacs='emacs -nw'     # on second thought... spacemacs actually makes graphical emacs pretty nice
 alias octave='octave --no-gui'
 
 # some more git tree shortcuts on top of those oh-my-zsh already provides
