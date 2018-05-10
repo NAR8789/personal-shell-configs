e732f8231d76ce861158b4d6e89ddcbceaf9c174
Xiao Fan <xiao.h.fan@gmail.com>
2018-05-10 11:20:21 -0700

    more context in cwd prompt--leave more than 1 char per dir

--- settings.zsh
+++ settings.zsh
@@ -1,4 +1,4 @@
 unsetopt beep
 
 # custom zsh git prompt (requires olivierverdier/zsh-git-prompt)
-PROMPT='%B%F{red}%m%F{default}:%F{cyan}$(shrink_path -f -T)%F{default}%b$(git_super_status)%B%#%b '
+PROMPT='%B%F{red}%m%F{default}:%F{cyan}$(shrink_path -l -T)%F{default}%b$(git_super_status)%B%#%b '
