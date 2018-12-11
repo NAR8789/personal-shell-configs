9b447d224263dc814341412f5a7eac72be4d3b3a
Xiao Fan <xiao.h.fan@gmail.com>
2018-12-10 17:50:19 -0800

    round brackets around zsh prompt

--- settings.zsh
+++ settings.zsh
@@ -2,3 +2,5 @@ unsetopt beep
 
 # custom zsh git prompt (requires olivierverdier/zsh-git-prompt)
 PROMPT='%B%F{red}%m%F{default}:%F{cyan}$(shrink_path -l -T)%F{default}%b$(git_super_status)%B%#%b '
+ZSH_THEME_GIT_PROMPT_PREFIX='('
+ZSH_THEME_GIT_PROMPT_SUFFIX=')'
