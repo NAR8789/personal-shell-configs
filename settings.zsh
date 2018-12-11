5ed81474a25db04efdd1436af58ff6b5ccb96eb3
Xiao Fan <xiao.h.fan@gmail.com>
2018-12-11 08:09:59 -0800

    experiment with more radical git status templates

--- settings.zsh
+++ settings.zsh
@@ -5,4 +5,10 @@ PROMPT='%B%F{red}%m%F{default}:%F{cyan}$(shrink_path -l -T)%F{default}%b$(git_su
 ZSH_THEME_GIT_PROMPT_BEHIND="↓"
 ZSH_THEME_GIT_PROMPT_AHEAD="↑"
 ZSH_THEME_GIT_PROMPT_LOCAL='L'
-ZSH_THEME_GIT_PROMPT='($branch$merge_or_rebase$upstream$behind$ahead|$staged$conflicts$changed$untracked$stashed$clean)'
+ZSH_GIT_PROMPT_SHOW_UPSTREAM=2
+ZSH_THEME_GIT_PROMPT_UPSTREAM_FRONT="%{$fg_bold[blue]%}"
+ZSH_THEME_GIT_PROMPT_UPSTREAM_END="%{${reset_color}%}"
+ZSH_THEME_GIT_PROMPT='(${(j:|:)${(s:|:)${:-$branch$merge_or_rebase|$upstream|$behind$ahead$staged$conflicts$changed$untracked$stashed$clean}}})'
+
+# saner line, don't delete me
+#ZSH_THEME_GIT_PROMPT='($branch$merge_or_rebase$upstream$behind$ahead|$staged$conflicts$changed$untracked$stashed$clean)'
