6a56bdd22f8b5f95770658af7ec04f69445b5d16
Xiao Fan <xiao.h.fan@gmail.com>
2018-12-10 22:20:40 -0800

    zsh theme git prompt template string

--- settings.zsh
+++ settings.zsh
@@ -2,7 +2,6 @@ unsetopt beep
 
 # custom zsh git prompt (requires olivierverdier/zsh-git-prompt)
 PROMPT='%B%F{red}%m%F{default}:%F{cyan}$(shrink_path -l -T)%F{default}%b$(git_super_status)%B%#%b '
-ZSH_THEME_GIT_PROMPT_PREFIX='('
-ZSH_THEME_GIT_PROMPT_SUFFIX=')'
-ZSH_THEME_GIT_PROMPT_BEHIND="%{↓%2G%}"
-ZSH_THEME_GIT_PROMPT_AHEAD="%{↑%2G%}"
+ZSH_THEME_GIT_PROMPT_BEHIND="↓"
+ZSH_THEME_GIT_PROMPT_AHEAD="↑"
+ZSH_THEME_GIT_PROMPT='($BRANCH$MERGING_OR_REBASE$UPSTREAM$BEHIND$AHEAD|$STAGED$CONFLICTS$CHANGED$UNTRACKED$STASHED$CLEAN)'
