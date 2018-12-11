52a3e9b95f0b464e1f2b8a57ac218173287261d1
Xiao Fan <xiao.h.fan@gmail.com>
2018-12-10 17:52:35 -0800

    more futzing with git prompt

--- settings.zsh
+++ settings.zsh
@@ -4,3 +4,5 @@ unsetopt beep
 PROMPT='%B%F{red}%m%F{default}:%F{cyan}$(shrink_path -l -T)%F{default}%b$(git_super_status)%B%#%b '
 ZSH_THEME_GIT_PROMPT_PREFIX='('
 ZSH_THEME_GIT_PROMPT_SUFFIX=')'
+ZSH_THEME_GIT_PROMPT_BEHIND="%{↓%2G%}"
+ZSH_THEME_GIT_PROMPT_AHEAD="%{↑%2G%}"
