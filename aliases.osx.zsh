58be552f9d3a0d21581cb9dc0cbccbbb11061fdd
Xiao Fan <2258776+NAR8789@users.noreply.github.com>
2021-10-28 08:57:14 -0700

    gls instead of ls on osx

--- aliases.osx.zsh
+++ aliases.osx.zsh
@@ -1,5 +1,9 @@
 if [[ "$OSTYPE" == "darwin"* ]]; then
-  alias ls='ls -FG'    # append indicator, colorized
+  alias ls="$(
+    BASE="$(dirname "${(%):-%N}")"
+    eval $(dircolors "$BASE/dircolors")
+    echo -n "LS_COLORS='$LS_COLORS' gls --classify --color=always"
+  )"
   alias find='find .'  # I usually just want to search in current directory. For explicit directory passing, use '\find'
   alias gg=gitx        # so I don't have to remember to type gitx vs gg on osx vs linux
 fi
