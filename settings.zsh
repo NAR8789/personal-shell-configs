3f2006db2b3f7dfcd0c47815a5872fa2c1490f7f
Xiao Fan <2258776+NAR8789@users.noreply.github.com>
2022-11-23 11:02:17 -0700

    move oh-my-zsh git prompt from RPROMPT to PROMPT

--- settings.zsh
+++ settings.zsh
@@ -3,4 +3,5 @@ unsetopt beep
 # allow for functions in the prompt
 setopt PROMPT_SUBST
 
-PROMPT='%B%F{red}%m%F{default}:%F{cyan}$(shrink_path --last --nameddirs)%F{default}%#%b '
+unset RPROMPT
+PROMPT='%B%F{red}%m%F{default}:%F{cyan}$(shrink_path --last --nameddirs)%F{default}%b$(git_super_status)%B%#%b '
