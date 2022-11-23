ba5fc5824ce19c5df13ed0cc218c5a407579378d
Xiao Fan <2258776+NAR8789@users.noreply.github.com>
2022-11-23 11:54:27 -0700

    show upstream in git prompt

--- settings.zsh
+++ settings.zsh
@@ -3,5 +3,5 @@ unsetopt beep
 # allow for functions in the prompt
 setopt PROMPT_SUBST
 
-unset RPROMPT
 PROMPT='%B%F{red}%m%F{default}:%F{cyan}$(shrink_path --last --nameddirs)%F{default}%b$(git_super_status)%B%#%b '
+ZSH_THEME_GIT_SHOW_UPSTREAM=short
