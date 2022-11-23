425971aff90c6ef67136a1649d960a5fe4c8300a
Xiao Fan <2258776+NAR8789@users.noreply.github.com>
2022-11-23 10:37:24 -0700

    remove my git prompt for now, as it hangs in worktrees

--- settings.zsh
+++ settings.zsh
@@ -1,8 +1,6 @@
 unsetopt beep
 
-# custom zsh git prompt (requires olivierverdier/zsh-git-prompt)
-PROMPT='%B%F{red}%m%F{default}:%F{cyan}$(shrink_path --last --nameddirs)%F{default}%b$(git_super_status)%B%#%b '
-ZSH_THEME_GIT_PROMPT_PREFIX='('
-ZSH_THEME_GIT_PROMPT_SUFFIX=')'
-ZSH_THEME_GIT_PROMPT_BEHIND="↓"
-ZSH_THEME_GIT_PROMPT_AHEAD="↑"
+# allow for functions in the prompt
+setopt PROMPT_SUBST
+
+PROMPT='%B%F{red}%m%F{default}:%F{cyan}$(shrink_path --last --nameddirs)%F{default}%#%b '
